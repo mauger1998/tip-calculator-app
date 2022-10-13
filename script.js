934684576edebe78e7a7805df8de9401847d3b10
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-13 21:38:42 +0100

    Write function to return total based on number of people

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const percentButtons = document.querySelectorAll(".buttonJava");
+const billPrice = document.getElementById("billPrice");
+const totalOutput = document.getElementById("total");
+const numberOfPeople = document.getElementById("numberOfPeople");
+
+function displayTotal(numberOfPeople, billPrice) {
+    let result = billPrice / numberOfPeople;
+    return result;
+}
