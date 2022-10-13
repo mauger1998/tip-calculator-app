9a32a2e169ebb60e24d3cf763ccf08615746722f
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-13 21:48:41 +0100

    Add functionality to display total for now

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const totalOutput = document.getElementById("total");
 const numberOfPeople = document.getElementById("numberOfPeople");
 
 function displayTotal(numberOfPeople, billPrice) {
-    let result = billPrice / numberOfPeople;
+    let result = billPrice.value / numberOfPeople.value;
     return result;
 }
+
+percentButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        totalOutput.textContent = displayTotal(numberOfPeople, billPrice);
+    })
+})
+    
+    
+
+
+
