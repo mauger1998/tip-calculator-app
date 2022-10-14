e5e0577715633dd6964d9e32a1d10e00904259fe
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-14 06:43:59 +0100

    Display Outputs
    Used .slice to take the percent sign off the button value, then used parseFloat to conver from string to a float and used backticks to put the number on the end of a decimal point for the calculation

--- script.js
+++ script.js
@@ -2,17 +2,35 @@ const percentButtons = document.querySelectorAll(".buttonJava");
 const billPrice = document.getElementById("billPrice");
 const totalOutput = document.getElementById("total");
 const numberOfPeople = document.getElementById("numberOfPeople");
+const tipOutput = document.getElementById("tipOutput");
 
-function displayTotal(numberOfPeople, billPrice) {
-    let result = billPrice.value / numberOfPeople.value;
-    return result;
-}
+
+
+
+
+
 
 percentButtons.forEach(button => {
     button.addEventListener("click", (e) => {
-        totalOutput.textContent = displayTotal(numberOfPeople, billPrice);
+        let amountToAdd = e.target.textContent.slice(0, -1);
+        tipOutput.textContent = displayTipAmount(billPrice, amountToAdd, numberOfPeople)
+        totalOutput.textContent = displayTotal(numberOfPeople, billPrice, amountToAdd);
     })
 })
+
+function displayTotal(numberOfPeople, billPrice, amountToAdd) {
+    let result = billPrice.value * parseFloat(`1.${amountToAdd}`) / numberOfPeople.value;
+
+      
+    
+    return result;
+}
+
+function displayTipAmount(billPrice, amountToAdd, numberOfPeople) {
+    let result = billPrice.value * parseFloat(`0.${amountToAdd}`) / numberOfPeople.value;
+    let newResult = result.toFixed(2);
+    return newResult;
+}
     
     
 
